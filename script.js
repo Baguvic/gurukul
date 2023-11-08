053c93f5a4d9c197e77329e2b28874dea05e03a4
baguvic <baguvichernandez@gmail.com>
2023-11-08 18:41:38 +0700

    nice try

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ document.addEventListener("click", function (e) {
     navbar.classList.remove("sidebar");
   }
 });
+
+const pertanyaanone = document.querySelector(".text-quest-1");
+document.querySelector("#show-1").onclick = () => {
+  pertanyaanone.classList.toggle("jawab-1");
+};
+
+const pertanyaantwo = document.querySelector(".text-quest-2");
+document.querySelector("#show-2").onclick = () => {
+  pertanyaantwo.classList.toggle("jawab-2");
+};
+
+const pertanyaanthree = document.querySelector(".text-quest-3");
+document.querySelector("#show-3").onclick = () => {
+  pertanyaanthree.classList.toggle("jawab-3");
+};
+
+const pertanyaanfour = document.querySelector(".text-quest-4");
+document.querySelector("#show-4").onclick = () => {
+  pertanyaanfour.classList.toggle("jawab-4");
+};
+
+const pertanyaanfive = document.querySelector(".text-quest-5");
+document.querySelector("#show-5").onclick = () => {
+  pertanyaanfive.classList.toggle("jawab-5");
+};
+
+const pertanyaansix = document.querySelector(".text-quest-6");
+document.querySelector("#show-6").onclick = () => {
+  pertanyaansix.classList.toggle("jawab-6");
+};
